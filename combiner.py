25477a55d990ef60c58b286d224ac0969025907a
Peiran Xiao <peiran.xiao@duke.edu>
2021-03-28 22:15:49 -0400

    update combiner.py

--- combiner.py
+++ combiner.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
-
 # This file combines all downloaded .bib and .bibtex files to a single .bib file (com.bib)
 
-import csv
 import glob
+import os 
+
+# set working dir
+# cwd = r'E:\Documents\test'
+# os.chdir(cwd)
+
 files = glob.glob('*.bib')
 files.extend(glob.glob('*.bibtex'))
-with open('com.bib', 'w') as result:
+
+with open('com.bib', 'w', encoding='utf-8') as result:
     for file in files:
-        if len(file)<=12: continue # skip the combined file, usually with a shorter file name (<=12 incl. extension)
-        for line in open( file, 'r' ):
-            if line != '\n':
-                result.write( line )
+        if len(file)<=12 | (sum(c.isdigit() for c in file)<=1): 
+            continue # skip the combined file, usually with a shorter file name (<=12 incl. the extension ".bib")
+        for line in open( file, 'r', encoding='utf-8'):
+            if line == '\n': continue
+            result.write( line )
         result.write('\n'*2)
