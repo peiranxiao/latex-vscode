66fe15918783ea14740809e6aafa9693e81d9fe8
Peiran Xiao <peiran.xiao@duke.edu>
2021-03-28 22:16:57 -0400

    update combiner.py

--- combiner.py
+++ combiner.py
@@ -14,7 +14,7 @@ files.extend(glob.glob('*.bibtex'))
 with open('com.bib', 'w', encoding='utf-8') as result:
     for file in files:
         if len(file)<=12 | (sum(c.isdigit() for c in file)<=1): 
-            continue # skip the combined file, usually with a shorter file name (<=12 incl. the extension ".bib")
+            continue # skip the combined file, usually with a shorter file name (<=12 incl. the extension ".bib") or contains few numbers (<=1) 
         for line in open( file, 'r', encoding='utf-8'):
             if line == '\n': continue
             result.write( line )
